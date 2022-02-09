a7fd97a184ba8dc0fb15dcb04c626e7fd1f4a820
swap-stack <swapnilakavishwar@gmail.com>
2022-02-09 19:54:11 +0530

    Added django-heroku for settings

--- exercise_app/exercise_app/settings.py
+++ exercise_app/exercise_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,7 +35,6 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'api',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -133,3 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
